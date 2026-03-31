68bbcf280e6607c938501cc1bf1af48457a7273f
Wellington Chida <wellingtonchida@protonmail.com>
2026-03-30 22:02:24 -0300

    refactor: modularize server creation and update payload schema to use question field

--- ia-aplicada/choice-models/src/index.ts
+++ ia-aplicada/choice-models/src/index.ts
@@ -1,29 +1,16 @@
-import Fastify from "fastify";
+import { createServer } from "./server.ts";
 
-const service = Fastify();
 
-service.post("/chat", async  (request, reply) => {
-    const { message } = request.body as { message: string };
-    console.log(message);
-});
-
-try{
-    await service.listen({ port: 3000 });
-    console.log("Server running on port 3000");
-}catch(error){
-    console.error(error);
-    process.exit(1);
-}
-
+const service = createServer();
 
 service.inject({
     method: "POST",
     url: "/chat",
     payload: {
-        message: "Hello"
+        question: "Hello"
     }
 }).then((response) => {
-    console.log(response);
+    console.log("resp:",response.body);
 });
 
 
